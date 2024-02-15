c931dae10b17ef0be49b2b4c6b57c66245ceac27
Lazar <dima.lazarev@cheitgroup.com>
2024-02-15 10:46:15 +0200

    init test task

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,34 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useNavigate, useLocation } from 'react-router-dom';
-import {gsap} from "gsap";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { useNavigate, useLocation } from 'react-router-dom';
+// import {gsap} from "gsap";
 
 import './app.scss';
 import Header from '../header/header';
-import WherePage from '../../pages/wherePage';
-import WhatPage from '../../pages/whatPage';
-import WhoPage from '../../pages/whoPage';
+// import WherePage from '../../pages/wherePage';
+// import WhatPage from '../../pages/whatPage';
+// import WhoPage from '../../pages/whoPage';
 import HomePage from "../../pages/homePage";
 import {data} from '../../utils/constants';
 
 const App = () => {
   return (
-      <Router>
-        <div className="App">
+      <div className="App">
           <Header data ={data}/>
-          <Routes>
-            <Route exact path="/" element={<HomePage data={data}/>} />
-            <Route path="/who" element={<WhoPage data={data}/>} />
-            <Route path="/what" element={<WhatPage data={data}/>} />
-            <Route path="/where" element={<WherePage data={data}/>} />
-          </Routes>
-        </div>
-      </Router>
+          <HomePage data={data}/>
+      </div>
+      // <Router>
+      //   <div className="App">
+      //     <Header data ={data}/>
+      //       <HomePage data={data}/>
+      //     <Routes>
+      //       <Route exact path="/" element={<HomePage data={data}/>} />
+      //       <Route path="/who" element={<WhoPage data={data}/>} />
+      //       <Route path="/what" element={<WhatPage data={data}/>} />
+      //       <Route path="/where" element={<WherePage data={data}/>} />
+      //     </Routes>
+      //   </div>
+      // </Router>
   );
 }
 
--- src/components/link/link.js
+++ src/components/link/link.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 const NavLink = ({url, label}) => {
     return (
-        <Link to={url} className="header_nav_link">{label}</Link>
+        // <Link to={url} className="header_nav_link">{label}</Link>
+        <a href={url} className="header_nav_link"> {label}</a>
     )
 };
 
