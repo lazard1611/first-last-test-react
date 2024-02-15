1844ef8451914bd7ef08c93e9f13c26e51728b57
Lazar <dima.lazarev@cheitgroup.com>
2024-02-15 11:41:46 +0200

    init test task

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const NavLink = ({url, label}) => {
     return (
         <Link to={url} className="header_nav_link">{label}</Link>
-        // <a href={url} className="header_nav_link"> {label}</a>
     )
 };
 
